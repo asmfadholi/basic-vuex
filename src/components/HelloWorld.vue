a0f622fb339e274b131a58e9652b6a96c7de5ef9
asmfadholi <asmfadholi@gmail.com>
2018-06-25 23:38:46 +0700

    beginner vuex

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="container">
+  <div class="text-center">
+    <h3>Clicked: {{ $store.state.count }} times, counter is {{ evenOrOdd }}</h3>
+
+    <button class="btn btn-success" @click='increment'> + </button>
+    <button class="btn btn-danger" @click='decrement'> - </button>
+
+    <button class="btn" @click='incrementIfOdd'> increment If Odd </button>
+    <button class="btn" @click='incrementAsync'> increment Async </button>
+  </div>
+
+</div>
+</template>
+
+<script>
+import {
+  mapGetters,
+  mapActions
+} from 'vuex'
+
+export default {
+  name: 'hello',
+  computed: mapGetters([
+    'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ]),
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
